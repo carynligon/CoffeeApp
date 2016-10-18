465ddcc11e655782b8716fa26dad81325b74d38c
Caryn Ligon <carynligon@gmail.com>
2016-10-18 11:13:58 -0500

    starting Redux store

--- app/scripts/Components/Timer.js
+++ app/scripts/Components/Timer.js
@@ -4,21 +4,20 @@ class Timer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {minutes: 0, seconds: 0, timerText:'00:00'};
+    this.startTimer = this.startTimer.bind(this);
   }
   addTime() {
-    console.log(this.state);
     if (this.state.seconds <= 58) {
-     this.state.seconds = this.state.seconds + 1;
+      this.setState({seconds: this.state.seconds + 1})
      if (this.state.seconds <= 9 && this.state.minutes <= 9) {
-       this.state.timerText = '0' + this.state.minutes + ':' + '0' + this.state.seconds;
+       this.setState({timerText: '0' + this.state.minutes + ':' + '0' + this.state.seconds})
      } else if (this.state.seconds >= 10 && this.state.minutes <= 9) {
-       this.state.timerText = '0' + this.state.minutes + ':' + this.state.seconds;
+       this.setState({timerText: '0' + this.state.minutes + ':' + this.state.seconds})
      } else {
-       this.state.timerText = this.state.minutes + ':' + this.state.seconds;
+       this.setState({timerText: this.state.minutes + ':' + this.state.seconds})
      }
     } else {
-     this.state.minutes = this.state.minutes + 1;
-     this.state.seconds = 0;
+      this.setState({minutes: this.state.minutes + 1, seconds: 0})
    }
    this.startTimer();
   }
@@ -34,7 +33,6 @@ class Timer extends React.Component {
     timer.textContent = '00:00';
   }
   render() {
-    console.log(this.state);
     return (
       <div id="timer-wrapper">
         <div id="timer">{this.state.timerText}</div>
--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -2,4 +2,19 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Timer from './Components/Timer';
 
+import store from './store';
+
 ReactDom.render(<Timer/>, document.getElementById('container'));
+
+// console.log(store.getState());
+//
+// const render = () => {
+//   document.body.innerText = store.getState();
+// }
+//
+// store.subscribe(render);
+// render();
+//
+// document.addEventListener('click', () => {
+//   store.dispatch({type:'INCREMENT'});
+// });
